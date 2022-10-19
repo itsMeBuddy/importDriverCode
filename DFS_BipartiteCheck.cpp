c7f3998abf8a0df35c3437704b0f33f9e0dde36f
m.ayank.r <red.mayank.r@gmail.com>
2022-10-19 20:28:44 +0530

    check if a graph is bipartite using DFS

---
+++ DFS_BipartiteCheck.cpp
@@ -0,0 +1,28 @@
+bool bipartiteDfs(int node, vector<int> adj[], int color[]){
+    if(color[node]==-1) color[node]=1;
+
+    for(auto it: adj[node]){
+        if(color[it]==-1){
+            color[it] = 1-color[node];
+            if(!bipartiteDfs(it,adj,color)){
+                return false;
+            }
+            else if(color[it]==color[node]) return false;
+        }
+    }
+    return true;
+}
+
+
+bool checkBipartite(vector<int> adj[], int n){
+    int color[n];
+    memset(color, -1, sizeof color);
+
+    for(int i=0;i<n;i++){
+        if(color[i]==-1){
+            if(!bipartiteDfs(i,adj,color)){
+                return false;
+            }
+        }
+    }
+}
